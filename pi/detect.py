af84b40e6c054e3a8d2b6bb9dfb4b242780b8893
Tommy Li <tommyli3318@gmail.com>
2020-02-01 11:59:12 -0800

    contour detection

--- pi/detect.py
+++ pi/detect.py
@@ -2,15 +2,37 @@ import cv2
 import numpy as np
 
 cap = cv2.VideoCapture(0)
+# fgbg = cv2.bgsegm.createBackgroundSubtractorMOG() # bg mask
+# fgmask = fgbg.apply(frame) # apply mask
+# cv2.imshow('With Bg Reduction',fgmask) # show mask
 
 while True:
 	# Read by frame
-	ret, frame = cap.read()
+	_, frame = cap.read()
+	gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+	flag, thresh = cv2.threshold(gray, 120, 255, cv2.THRESH_BINARY) # flag is useless
 
-	# Display the frame
-	cv2.imshow('frame',frame)
 
-	if cv2.waitKey(50) & 0xFF == ord('q'):
+	# Find contours
+	contours, _ = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+	contours = sorted(contours, key=cv2.contourArea,reverse=True)
+	# Select long perimeters only
+	perimeters = [cv2.arcLength(contours[i],True) for i in range(len(contours))]
+	listindex=[i for i in range(15) if perimeters[i]>perimeters[0]/2]
+	numcards=len(listindex)
+
+	# Display
+	cv2.imshow('Normal',frame)
+	cv2.imshow("Gray",gray)
+	cv2.imshow("Thresh",thresh)
+
+	copy = frame.copy()
+	[cv2.drawContours(copy, [contours[i]], 0, (0,255,0), 5) for i in listindex]
+	cv2.imshow("Contours", copy)
+	
+
+
+	if cv2.waitKey(50) == ord('q'):
 		break
 
 
