6dc433680d0a164ed47d33366ffa3168d9acffa7
costajoshua27 <costa.joshua27@gmail.com>
2020-02-01 14:23:40 -0800

    improved detection

--- pi/detect.py
+++ pi/detect.py
@@ -5,6 +5,7 @@ cap = cv2.VideoCapture(0)
 # fgbg = cv2.bgsegm.createBackgroundSubtractorMOG() # bg mask
 # fgmask = fgbg.apply(frame) # apply mask
 # cv2.imshow('With Bg Reduction',fgmask) # show mask
+j = 0
 
 while True:
 	# Read by frame
@@ -12,17 +13,19 @@ while True:
 	gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
 
 	# threshold(param, thresh_val, fill_color, type)
-	_, thresh = cv2.threshold(gray, 150, 225, cv2.THRESH_BINARY)
+	_, thresh = cv2.threshold(gray, 100, 225, cv2.THRESH_BINARY)
 
 
 	# Find contours
 	contours, _ = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 	# filter out the small ones
 	contours = [c for c in contours if cv2.contourArea(c) > 10000]
-
+	if(contours):
+		print("Collision detected: ",j)
+		j += 1
+		
 	# Display
 	cv2.imshow('Normal',frame)
-	cv2.imshow("Gray",gray)
 	cv2.imshow("Thresh",thresh)
 
 	for i in range(len(contours)):
