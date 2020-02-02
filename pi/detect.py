63a2a24879b24185eec74905f9f798ebea968658
Vikram Rajan <vikramrajan90@gmail.com>
2020-02-01 21:02:13 -0800

    When obstruction detected, beep sound plays

--- pi/detect.py
+++ pi/detect.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import subprocess
 from firebase import firebase
 
 
@@ -23,6 +24,7 @@ while True:
 	if contours:
 		# update database
 		fb.put('/safeskate-499c0/obs_table', 'obstruction', 1)
+		p = subprocess.Popen(["afplay", "beep.mp3"])
 	else:
 		# update database
 		fb.put('/safeskate-499c0/obs_table', 'obstruction', 0)
