445414ac389c92c01d33b5cb7dd5530d6120b93e
Tommy Li <tommyli3318@gmail.com>
2020-02-01 16:51:19 -0800

    cleanups

--- pi/detect.py
+++ pi/detect.py
@@ -6,10 +6,6 @@ from firebase import firebase
 fb = firebase.FirebaseApplication("https://safeskate-499c0.firebaseio.com/", None)
 
 cap = cv2.VideoCapture(0)
-# fgbg = cv2.bgsegm.createBackgroundSubtractorMOG() # bg mask
-# fgmask = fgbg.apply(frame) # apply mask
-# cv2.imshow('With Bg Reduction',fgmask) # show mask
-j = 0
 
 while True:
 	# Read by frame
@@ -24,11 +20,11 @@ while True:
 	contours, _ = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 	# filter out the small ones
 	contours = [c for c in contours if cv2.contourArea(c) > 10000]
-	if(contours):
-		print("Collision detected: ",j)
-		j += 1
+	if contours:
+		# update database
 		fb.put('/safeskate-499c0/obs_table', 'obstruction', 1)
 	else:
+		# update database
 		fb.put('/safeskate-499c0/obs_table', 'obstruction', 0)
 
 	# Display
@@ -36,14 +32,15 @@ while True:
 	cv2.imshow("Thresh",thresh)
 
 	for i in range(len(contours)):
-		cv2.drawContours(frame, [contours[i]], 0, (0,255,0), 5)
+		cv2.drawContours(frame, [contours[i]], 0, (50,50,255), 5)
 	cv2.imshow("Contours", frame)
 
 	if cv2.waitKey(50) == ord('q'):
 		break
 
-
+# update one last time before script ends
 fb.put('/safeskate-499c0/obs_table', 'obstruction', 0)
+
 # release everything afterwards
 cap.release()
 cv2.destroyAllWindows()
