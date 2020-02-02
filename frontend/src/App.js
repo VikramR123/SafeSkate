262d6e2bf1e8eccffd88cc460cad2dd830761777
Joshua Costa <costa.joshua27@gmail.com>
2020-02-01 16:28:35 -0800

    got obstruction detection working

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import Detector from './components/Detector';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <Navbar/>
+        <Detector/>
+    </div>
+  );
+}
+
+export default App;
